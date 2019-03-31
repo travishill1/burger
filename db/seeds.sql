9266c32ce36348931951d1579b564d6a7329821a
Travis <travishill1@gmail.com>
2019-03-31 17:36:42 -0500

    added some skeleton code and MVC organization

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dog Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+
+select * from burgers;
